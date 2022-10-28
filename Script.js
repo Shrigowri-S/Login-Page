4f77579cb64e25bb2449a09cdf7c9a16ed7d7259
Shrigowri S <shrigowris17@gmail.com>
2022-10-28 12:14:33 +0530

    First

--- Script.js
+++ Script.js
@@ -1,13 +1,28 @@
 "usestrict";
-
+let localUserName = "";
+let localPassword = "";
+let visibile = document.querySelector(".home");
 validate = function () {
   let userName = document.getElementById("username").value;
+
   let password = document.getElementById("password").value;
-  console.log(username, password);
-  if (userName && password) {
+  localStorage.setItem("username", userName);
+  localStorage.setItem("password", password);
+  localUserName = localStorage.getItem("username");
+  localPassword = localStorage.getItem("password");
+  console.log(localPassword, localUserName);
+  if (localPassword && localUserName) {
+    // visibile.style.visibility = "visible";
+
     window.location = "home.html";
+  } else {
+    alert("Username or Password id missing");
+    // visibile.style.cvisibility = "hidden";
   }
 };
 logout = function () {
+  localStorage.clear();
+  // visibile.style.visibility = "hidden";
+
   window.location = "login.html";
 };
