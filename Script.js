c370bc3d43e3b6fba4ca37de9a0b39022d09d6cc
Shrigowri S <shrigowris17@gmail.com>
2022-10-28 13:43:22 +0530

    First

--- Script.js
+++ Script.js
@@ -6,22 +6,22 @@ validate = function () {
   let userName = document.getElementById("username").value;
 
   let password = document.getElementById("password").value;
-  localStorage.setItem("username", userName);
-  localStorage.setItem("password", password);
-  localUserName = localStorage.getItem("username");
-  localPassword = localStorage.getItem("password");
+  sessionStorage.setItem("username", userName);
+  sessionStorage.setItem("password", password);
+  localUserName = sessionStorage.getItem("username");
+  localPassword = sessionStorage.getItem("password");
   console.log(localPassword, localUserName);
   if (localPassword && localUserName) {
     // visibile.style.visibility = "visible";
 
     window.location = "home.html";
   } else {
-    alert("Username or Password id missing");
+    alert("Username or Password is missing");
     // visibile.style.cvisibility = "hidden";
   }
 };
 logout = function () {
-  localStorage.clear();
+  sessionStorage.clear();
   // visibile.style.visibility = "hidden";
 
   window.location = "login.html";
