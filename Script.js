a86aca5ac1eea37fde3c7f0038bcdad641ead0e1
Shrigowri S <shrigowris17@gmail.com>
2022-11-02 13:36:32 +0530

    First

--- Script.js
+++ Script.js
@@ -1,28 +1,25 @@
 "usestrict";
-let localUserName = "";
-let localPassword = "";
-let visibile = document.querySelector(".home");
+
+let interfacePage = function () {
+  if (localStorage.length != 0) {
+    window.location = "home.html";
+  } else {
+    window.location = "login.html";
+  }
+};
 validate = function () {
   let userName = document.getElementById("username").value;
 
   let password = document.getElementById("password").value;
-  sessionStorage.setItem("username", userName);
-  sessionStorage.setItem("password", password);
-  localUserName = sessionStorage.getItem("username");
-  localPassword = sessionStorage.getItem("password");
-  console.log(localPassword, localUserName);
-  if (localPassword && localUserName) {
-    // visibile.style.visibility = "visible";
-
-    window.location = "home.html";
+  if (userName && password) {
+    localStorage.setItem("username", userName);
+    localStorage.setItem("password", password);
   } else {
     alert("Username or Password is missing");
-    // visibile.style.cvisibility = "hidden";
   }
-};
-logout = function () {
-  sessionStorage.clear();
-  // visibile.style.visibility = "hidden";
+  // console.log(localPassword, localUserName);
+  document.getElementById("username").value = "";
 
-  window.location = "login.html";
+  document.getElementById("password").value = "";
+  interfacePage();
 };
